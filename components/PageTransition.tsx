c5d1c2ee1572057b2064d6d91ace30a0fc76ae20
TeddyMuli <tmuli974@gmail.com>
2024-05-29 10:14:48 +0300

    added page transition

---
+++ components/PageTransition.tsx
@@ -0,0 +1,11 @@
+const PageTransition = ({ children }: Readonly<{
+    children: React.ReactNode;
+  }>) => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default PageTransition;
