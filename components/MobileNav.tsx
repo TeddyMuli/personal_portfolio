64949c87276c5469668ac052fc9e54cf09317be3
TeddyMuli <tmuli974@gmail.com>
2024-05-29 14:43:49 +0300

    added constant to the MobileNav component

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,8 +1,16 @@
+"use client"
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/navigation";
+import { CiMenuFries } from "react-icons/ci";
+import { links } from "@/constants";
+
 const MobileNav = () => {
     return (
-        <div>
+        <nav>
             mobile nav
-        </div>
+        </nav>
     );
 }
 
