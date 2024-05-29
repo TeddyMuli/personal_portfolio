45b075bcdc98a96f7c2079386396a7ebf38196e7
TeddyMuli <tmuli974@gmail.com>
2024-05-29 13:22:24 +0300

    stairs transition working

--- components/Stairs.tsx
+++ components/Stairs.tsx
@@ -1,4 +1,5 @@
 import { animate, motion } from "framer-motion";
+
 const stairAnimation = {
     initial: {
         top: "0%",
@@ -22,13 +23,21 @@ const Stairs = () => {
         <>
             {/** Render 6 motion divs each representing a step of the stairs  */}
             {[...Array(6)].map((_, index) => {
-                <motion.div 
-                    key={index}
-                    variants={stairAnimation}
-                    initial="initial"
-                    animate="animate" 
-                    exit="exit"
-                />
+                return (
+                    <motion.div 
+                        key={index}
+                        variants={stairAnimation}
+                        initial="initial"
+                        animate="animate" 
+                        exit="exit"
+                        transition={{
+                            duration: 0.4,
+                            ease: "easeInOut",
+                            delay: reverseIndex(index) * 0.1,
+                        }}
+                        className="h-full w-full bg-white relative"
+                    />
+                )
             })}
         </>
     );
