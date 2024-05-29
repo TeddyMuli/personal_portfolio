a1460e0b01e93b6720a5b09b2d54d6829878cef1
TeddyMuli <tmuli974@gmail.com>
2024-05-29 14:43:27 +0300

    added constant to the Nav component

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,14 +1,7 @@
 "use client"
 import Link from "next/link"
 import { usePathname } from "next/navigation";
-
-const links = [
-    { name: "home", path: "/" },
-    { name: "services", path: "/services" },
-    { name: "resume", path: "/resume" },
-    { name: "work", path: "/work" },
-    { name: "contact", path: "/contact"}
-]
+import { links } from "@/constants";
 
 const Nav = () => {
     const pathname = usePathname();
