8c78061aafcb4d3aeabb84d7cf2aa2cefa30e505
TeddyMuli <tmuli974@gmail.com>
2024-05-29 13:37:59 +0300

    added StairsTransition component to layout.tsx

--- components/Stairs.tsx
+++ components/Stairs.tsx
@@ -31,7 +31,7 @@ const Stairs = () => {
                         animate="animate" 
                         exit="exit"
                         transition={{
-                            duration: 0.4,
+                            duration: 0.2,
                             ease: "easeInOut",
                             delay: reverseIndex(index) * 0.1,
                         }}
