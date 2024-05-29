0b005dcfa84205bad13874aeaab7f3235224d046
TeddyMuli <tmuli974@gmail.com>
2024-05-29 17:51:46 +0300

    created socials component

---
+++ components/Socials.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import { FaGithub, FaLinkedin } from "react-icons/fa"
+
+const socials = [
+    { icon: <FaGithub />, link: "/#" },
+    { icon: <FaLinkedin />, link: "/#" }
+]
+
+const Socials = ({ containerStyles, icons } : {
+    containerStyles: any, icons: any
+}) => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Socials;
