2ec8484adfd16fec5e91825d2784681c81a7a71f
TeddyMuli <tmuli974@gmail.com>
2024-05-29 13:24:58 +0300

    added StairsTransition component to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetbrainsMono = JetBrains_Mono({ 
   subsets: ["latin"],
@@ -24,6 +25,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={jetbrainsMono.variable}>
         <Header />
+        <StairTransition />
         <PageTransition>
           {children}
         </PageTransition>
