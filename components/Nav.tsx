452d7d864909113216303076c440ec02b0526d6f
TeddyMuli <tmuli974@gmail.com>
2024-05-29 09:33:11 +0300

    create desktop nav

---
+++ components/Nav.tsx
@@ -0,0 +1,27 @@
+"use client"
+import Link from "next/link"
+import { usePathname } from "next/navigation";
+
+const links = [
+    { name: "home", path: "/" },
+    { name: "services", path: "/services" },
+    { name: "resume", path: "/resume" },
+    { name: "work", path: "/work" },
+    { name: "contact", path: "/contact"}
+]
+
+const Nav = () => {
+    return (
+        <nav className="flex gap-4">
+           {links.map((link, index) => {
+            return (
+                <Link href={link.path} key={index}>
+                    {link.name}
+                </Link>
+            )
+           })}
+        </nav>
+    );
+}
+
+export default Nav;
