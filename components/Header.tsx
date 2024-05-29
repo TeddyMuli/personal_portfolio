ae7796807bdbb040c40322d523ff91b1a4acc7f4
TeddyMuli <tmuli974@gmail.com>
2024-05-29 09:32:52 +0300

    create header

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import React from 'react';
+import Nav from './Nav';
+import { Button } from './ui/button';
+
+const Header = () => {
+    return (
+        <header className='py-8 xl:py-12 text-white'>
+            <div className="container mx-auto flex justify-between items-center">
+                {/** Logo */}
+                <Link href='/'>
+                    <h1 className='text-4xl font-semibold'>
+                        Ted<span className='text-accent'>.</span>
+                    </h1>
+                </Link>
+                {/** desktop nav */}
+                <div className="hidden xl:flex items-center gap-8">
+                    <Nav />
+                    <Link href={'/contact'}>
+                        <Button>Hire me</Button>
+                    </Link>
+                </div>
+
+                {/** mobile nav */}
+                <div className="xl:hidden">
+                    mobile nav
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
