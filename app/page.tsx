36044620ae23912733fcd8c9efd904116861a6db
TeddyMuli <tmuli974@gmail.com>
2024-05-30 21:43:35 +0300

    modified home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,49 @@
-export default function Home() {
+import Photo from "@/components/Photo";
+import Socials from "@/components/Socials";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi"
+
+const Home = () => {
   return (
-    <main>
-      homepage
-    </main>
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/** text */}
+          <div className="text-center xl:text-left gap-4 order-2 xl:order-none">
+            <span className="text-xl">Software Engineer</span>
+            <h1 className="h1 my-4">
+              Hi, I'm <div className="mt-2 text-accent">Teddy Muli<span className="text-white">.</span></div>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting elegant digital experiences and 
+              I am profecient in various programming languages and technologies
+            </p>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button 
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Socials 
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all"
+                />
+              </div>
+            </div>
+          </div>
+
+          {/**photo */}
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
+
+export default Home;
