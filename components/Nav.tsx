5838fa17cd80e6a6049ff68ec793d90e20820298
TeddyMuli <tmuli974@gmail.com>
2024-05-30 21:42:02 +0300

    modified Nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -9,7 +9,11 @@ const Nav = () => {
         <nav className="flex gap-8">
            {links.map((link, index) => {
             return (
-                <Link href={link.path} key={index} className={`${link.path === pathname && "text-accent border-b-2 border-accent"} capitalize font-medium hover:text-accent transition-all`}>
+                <Link 
+                    href={link.path}
+                    key={index}
+                    className={`${link.path === pathname && "text-accent border-b-2 border-accent"} capitalize font-medium hover:text-accent transition-all`}
+                >
                     {link.name}
                 </Link>
             );
